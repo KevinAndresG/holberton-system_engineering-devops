be0fcc7871447cf10ffc8aa54cf971767a3d0645
KevinAndresG <3348@holbertonschool.com>
2022-01-03 18:35:12 -0500

    Creating the directory and the first file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a new file in tmp
+file { 'school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data'
+  content => 'I love Puppet',
+}
