e8b30d9f2f3a1021223b6899d17668f4c05f9cb9
KevinAndresG <3348@holbertonschool.com>
2022-01-31 17:20:53 -0500

    fixing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,26 +7,24 @@
 import requests
 from sys import argv
 
-api_user = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-response = requests.get(api_user)
-api_todo = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-    argv[1])
-response2 = requests.get(api_todo)
-if response2.status_code == 200 & response.status_code == 200:
-    """ print(response.json()) """
-    emp_name = response.json()["name"]
-    completed = 0
-    todo = 0
-    for i in response2.json():
-        if i["completed"] is True:
-            completed += 1
-        todo += 1
-    print(f"Employee {emp_name} is done with tasks({completed}/{todo})")
-    list_todo = 0
-    for f in response2.json():
-        if f["completed"] is True:
-            print("\t {}".format(response2.json()[list_todo]["title"]))
-        list_todo += 1
-print(__import__("0-gather_data_from_an_API").__doc__)
 if __name__ == "__main__":
-    pass
+    api_user = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response = requests.get(api_user)
+    api_todo = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        argv[1])
+    response2 = requests.get(api_todo)
+    if response2.status_code == 200 & response.status_code == 200:
+        """ print(response.json()) """
+        emp_name = response.json()["name"]
+        completed = 0
+        todo = 0
+        for i in response2.json():
+            if i["completed"] is True:
+                completed += 1
+            todo += 1
+        print(f"Employee {emp_name} is done with tasks({completed}/{todo})")
+        list_todo = 0
+        for f in response2.json():
+            if f["completed"] is True:
+                print("\t {}".format(response2.json()[list_todo]["title"]))
+            list_todo += 1
