d20db888e92806434124fabe100d407c0864d90a
KevinAndresG <3348@holbertonschool.com>
2022-02-01 15:28:11 -0500

    fixing

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,22 +7,22 @@ import csv
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    api_user = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response = requests.get(api_user)
+    user_dict = response.json()
+    api_todos = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        argv[1])
+    response2 = requests.get(api_todos)
+    todos_dict = response2.json()
+    count = 0
 
-api_user = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-response = requests.get(api_user)
-user_dict = response.json()
-api_todos = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-    argv[1])
-response2 = requests.get(api_todos)
-todos_dict = response2.json()
-count = 0
-
-for x in response2.json():
-    data = []
-    a = [argv[1], user_dict["username"]]
-    b = [todos_dict[count]["completed"], todos_dict[count]["title"]]
-    c = a + b
-    count += 1
-    with open("{}.csv".format(argv[1]), 'a+') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerow(c)
+    for x in response2.json():
+        data = []
+        a = [argv[1], user_dict["username"]]
+        b = [todos_dict[count]["completed"], todos_dict[count]["title"]]
+        c = a + b
+        count += 1
+        with open("{}.csv".format(argv[1]), 'a+') as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            writer.writerow(c)
