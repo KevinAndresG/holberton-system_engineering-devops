5b0ea08d457b8937ca1b8f1448d289ca38cf55fd
KevinAndresG <3348@holbertonschool.com>
2022-02-16 23:03:17 -0500

    fixing

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# create a manifest that kills a process
+# fixing the apache sintax
 exec {'Fix an apache server':
   command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
   path    => '/usr/bin:/usr/sbin:/bin'
