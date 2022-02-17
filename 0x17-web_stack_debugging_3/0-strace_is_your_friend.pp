a500e72fad0027e0ed8d44e1e583ba14c7f2197a
KevinAndresG <3348@holbertonschool.com>
2022-02-16 22:57:04 -0500

    fixing a apache server

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # create a manifest that kills a process
 exec {'Fix an apache server':
   command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
-  path    => '/usr/sbin/'
+  path    => '/usr/bin/usr/sbin/'
 }
